de3fd17944ecda0e1945f6da66931987c2d9073b
yassine <chliguiy@gmail.com>
2025-07-25 00:43:11 +0200

    Updated CategoryManagement.tsx

--- src/components/admin/CategoryManagement.tsx
+++ src/components/admin/CategoryManagement.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Plus, Edit, Trash2, FolderOpen } from 'lucide-react';
 import { Category } from '../types';
-import { AdminApiService } from '../utils/adminApi';
+import { AdminApiService } from './../../utils/adminApi';
 import { ApiService } from '../utils/api';
 
 const CategoryManagement: React.FC = () => {
